48438366b2a15874f0405ac8994067b47a7ad021
Gordon_Palagi <89727193+GordonPalagi@users.noreply.github.com>
2023-05-03 20:46:26 -0700

    Add dropdown menu

--- riviera-style-co/src/Components/Layout/Navigation.js
+++ riviera-style-co/src/Components/Layout/Navigation.js
@@ -1,9 +1,57 @@
 import React from 'react'
 import '../CSS/NavCss.css'
+import { useState } from 'react';
+import beauty from '../Images/beautyAndBrains.avif'
+
 
 function Navigation() {
 
     const navBarLinks = ["Womens", "Mens", "Shoes", "Accessories"]
+
+    const [isHovered, setIsHovered] = useState(false);
+    const [hoveredLink, setHoveredLink] = useState(false);
+
+    const handleMouseOver = (index) => {
+        setIsHovered(true);
+        setHoveredLink(index);
+    }
+    const handleMouseOut = () => {
+        setIsHovered(false);
+        setHoveredLink(null);
+    }
+
+    const BannerAd = () => {
+        return (
+            <div className='bannerAd'>
+                Limited Time 
+                <span className='adStyle'> 20% off for Mothers Day!</span>
+            </div>
+        )
+    }
+
+
+    const BannerHover = ({array}) => {
+        
+
+
+        return (
+            <div className='hover-wrapper'>
+                <div className='bannerAd'>
+                    Limited Time 
+                    <span className='adStyle'> 20% off for Mothers Day!</span>
+                </div>
+                <div className='hovered-box-con'>
+                    <div className='li-con'>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat.</li>
+                    </div>
+                    <img src={beauty} alt="" />
+                </div>
+            </div>
+        )
+    }
+
 
     const Banner = ({title}) => {
 
@@ -13,11 +61,15 @@ function Navigation() {
                     <div className='title'>{title}</div>
                     {navBarLinks.map((item) => {
                         return (
-                            <div className='item'>{item}</div>
+                                <div 
+                                    onMouseOver={handleMouseOver}
+                                    onMouseOut={handleMouseOut} 
+                                    className='item'>{item}
+                                </div>
                             )
                         })}
                 </div>
-                <div className='bannerAd'>Limited Time <span className='adStyle'> 20% off for Mothers Day!</span></div>
+                {isHovered ? <BannerHover/> : <BannerHover/>}
             </>
         )
     }
